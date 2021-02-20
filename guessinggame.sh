af5cdb885d535bb7ad3d438ef73634b68a499cbc
racheagle <79341593+racheagle@users.noreply.github.com>
2021-02-19 16:13:05 -0800

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,20 @@
-
+echo "Guessing Game"
+function guess {
+    echo "Enter the number of files that's in the current directory:"
+    read answer
+    files=$(ls -1 | wc -l)
+}
+guess
+while [[$answer -ne $files]]
+do
+    if [[$answer -lt $files]] 
+    then
+        echo "Too low. :( Please try again!"
+    else
+        echo "Too high. :( Please try again!"
+    fi
+    guess
+done
+echo "You got it! :)"
+echo "==" && ls -1
+echo "Thanks for playing!"
